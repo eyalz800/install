cb227718352ea89311197f7173334c9c126de397
eyalz800 <eyalz800@gmail.com>
2024-02-11 18:05:04 +0200

    Install ignore sudo password

--- install.sh
+++ install.sh
@@ -2,6 +2,28 @@
 
 set -e
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo chattr -i /tmp/sudoers 2>/dev/null || true
+    sudo chattr -i /tmp/sudoers_backup 2> /dev/null || true
+    sudo cp /etc/sudoers /tmp/sudoers
+    sudo cp /etc/sudoers /tmp/sudoers_backup
+    sudo chattr -i /tmp/sudoers 2> /dev/null || true
+    sudo chmod 777 /tmp/sudoers
+    sudo echo $USER 'ALL=(ALL:ALL) NOPASSWD: ALL' >> /tmp/sudoers
+    sudo chmod 440 /tmp/sudoers
+    sudo chattr +i /tmp/sudoers 2> /dev/null || true
+    sudo chattr +i /tmp/sudoers_backup 2>/dev/null || true
+    trap 'sudo chattr -i /etc/sudoers 2>/dev/null || true ; \
+          sudo chattr -i /tmp/sudoers_backup 2>/dev/null || true ; \
+          sudo mv /tmp/sudoers_backup /etc/sudoers ; \
+          sudo chattr +i /etc/sudoers 2>/dev/null || true' \
+          EXIT
+    sudo chattr -i /tmp/sudoers 2> /dev/null || true
+    sudo chattr -i /etc/sudoers 2> /dev/null || true
+    sudo mv /tmp/sudoers /etc/sudoers
+    sudo chattr +i /etc/sudoers 2> /dev/null || true
+fi
+
 rm -rf nvim-install
 rm -rf tmux-conf
 rm -rf zsh
